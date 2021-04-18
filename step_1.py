41a6244475b688df2a5b2bd3ef1a72d52fce36b2
IgorStasyuk <stasyuk@bk.ru>
2021-04-18 19:44:22 +0300

    Initial commit

---
+++ step_1.py
@@ -0,0 +1,13 @@
+number1 = 1
+number2 = 3
+number3 = 5
+number4 = 7
+print(number1)
+print(number2)
+print(number3)
+print(number4)
+print('Введите ваше имя')
+print(input())
+print('Введите ваш возраст')
+print(int(input()))
+
